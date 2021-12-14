f096cb215a6eef37d7276fbcd3cc0c4864d0b9ce
Giang <463189@student.fontys.nl>
2021-12-14 16:44:16 +0100

    Add Unit test

--- CollegeNetwork.tests/UserTests.cs
+++ CollegeNetwork.tests/UserTests.cs
@@ -1,34 +1,37 @@
+using System;
 using CollegeNetwork.tests.stubData;
+using CollegeNetworkBackend1.Lib.Logic.User;
 using NUnit.Framework;
 
 namespace CollegeNetwork.tests
-
-public class Tests
 {
-    [Test]
-    public void verifyLogin_MockUsers_AreEqual()
+    public class Tests
     {
-        // Arrange
-        AuthControllerStub authControllerStub = new();
-        CustomerContainer customerContainer = new(customerDalStub);
+  
+    
+        private readonly User validUser1 = new User()
+        {  
+            id = 4, 
+            firstName = "fourth", 
+            lastName = "Duke", 
+            email = "Damm@won.net",
+            phoneNumber = "0694813584", 
+            dateOfBirth = new DateTime(1999, 7, 05), 
+            gender = "other",
+            password = "bruh3", 
+            isAdmin = false
+        };
+    
+        [Test]
+        public void getById_MockUsers_AreEqual()
+        {
+        
+            // Arrange
+            AuthControllerStub authControllerStub = new();
+            //UserRepository userRepository = new()
 
-        string validEmail1 = "Tem@live.com";
-        string validPassword1 = "Tommy12";
 
-        string validEmail2 = "Tem@live.com";
-        string invalidPassword1 = "randomTest2";
 
-        string invalidEmail1 = "lolll@gmail.com";
-        string validPassword2 = "TRran4";
-
-        string validEmail3 = "Wellempje@game.net";
-        string validPassword3 = "rompot3";
-
-        // Act & Assert
-        Assert.DoesNotThrow(() => customerContainer.verifyLogin(validEmail1, validPassword1));
-        Assert.Catch<CustomerLoginException>(() => customerContainer.verifyLogin(validEmail2, invalidPassword1));
-        Assert.Catch<InvalidOperationException>(() => customerContainer.verifyLogin(invalidEmail1, validPassword2));
-        Assert.DoesNotThrow(() => customerContainer.verifyLogin(validEmail3, validPassword3));
-
+        }
     }
 }
--- CollegeNetwork.tests/stubData/AuthControllerStub.cs
+++ CollegeNetwork.tests/stubData/AuthControllerStub.cs
@@ -1,7 +1,66 @@
-﻿namespace CollegeNetwork.tests.stubData
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using CollegeNetworkBackend1.Lib.Data;
+using CollegeNetworkBackend1.Lib.Logic.User;
+
+namespace CollegeNetwork.tests.stubData
 {
-    public class AythControllerStub
+    public class AuthControllerStub : IUserRepository
     {
-        
+        private List<User> userList = new()
+        {
+            new User
+            {
+                id = 1, firstName = "Tom", lastName = "Hanks", email = "Tommy@live.nl", phoneNumber = "0695847911",
+                dateOfBirth = new DateTime(2000, 12, 21), gender = "Male", password = "test", isAdmin = false
+            },
+            new User
+            {
+                id = 2, firstName = "Roos", lastName = "dijck", email = "Roosje@hart.nl", phoneNumber = "0694836745",
+                dateOfBirth = new DateTime(1988, 5, 3), gender = "female",
+                password = "53B945B392DFC50FBDA2B803A0F9E7388420F094665BD31A1AE22DBC3D1F5482", isAdmin = false
+            },
+            new User
+            {
+                id = 3, firstName = "Willy", lastName = "Alex", email = "Wellempje@game.net",
+                phoneNumber = "0694856384", dateOfBirth = new DateTime(1995, 7, 18), gender = "other",
+                password = "2AEB9015B14196CCA87359FE32103AC60593D5E9F78793298F001E2225B17877", isAdmin = false
+            }
+        };
+
+        public User create(User user)
+        {
+            int index = userList.FindLastIndex(c => c.id == user.id);
+            if (index != -1)
+            {
+                userList[index] = new User()
+                {
+                    id = user.id,
+                    firstName = user.firstName,
+                    lastName = user.lastName,
+                    email = user.email,
+                    phoneNumber = user.phoneNumber,
+                    dateOfBirth = user.dateOfBirth,
+                    gender = user.gender,
+                    password = user.password,
+                    isAdmin = user.isAdmin
+                };
+            }
+
+            return user;
+        }
+
+        public User getByEmail(string email)
+        {
+            User user = userList.Single(x => x.email == email);
+            return user;
+        }
+
+        public User getById(int id)
+        {
+            User user = userList.Single(x => x.id == id);
+            return user;
+        }
     }
 }
